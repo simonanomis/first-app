73d1014ac067a1ecae6b5cf81c8762cc6800b7b4
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2020-05-09 21:50:13 +0200

    added logger module and call it in app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+const log = require('./logger');
+
 function sayHello(name) {
     console.log('Hello ' + name + ' from your first NodeJS app');
 }
 
-sayHello('Simona');
+//sayHello('Simona');
+//console.log(module);
+
+log('message');
+
